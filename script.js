1c8209ee447673507aeb71361f4fa43252687ae1
LegoFigure11 <legofigurefb@gmail.com>
2017-04-20 16:05:56 +1000

    Update script.js

--- script.js
+++ script.js
@@ -157,6 +157,26 @@ var savecustom = function()
 			var move3 = movlist.split(",")[2]
 			var move4 = movlist.split(",")[3]
 			
+			else var spiv = ln10p1.concat(IVs[5], lnbr);
+	{
+		var pivs = ""
+    		if ((IVs[0] + IVs[1] + IVs[2] + IVs[3] + IVs[4] + IVs[5])<186
+		var pivs = '"ivs":{'
+	}
+    
+  		if (document.getElementById("hcheck").checked) = true
+    		var p1 = '{"'
+    		var p2 = '":{"flags":{'
+    		var p3 = '},"sets":['
+    		var hcheck = p1.concat(species, p2, flags, p3)
+    		else var hcheck = "";
+    
+    		var bcheck = "";
+    
+    		if (document.getElementById("fcheck").checked) = true
+    		var fcheck = ']},'
+		
+			
 		    if (lines[0].indexOf('(') != -1)
 		    {
 		    var ln2p2 = lines[0].substring(0, lines[0].indexOf('(')).trim()
@@ -252,24 +272,6 @@ var savecustom = function()
 		{
 			var spiv = ""
 		}
-		else var spiv = ln10p1.concat(IVs[5], lnbr);
-	{
-		var pivs = ""
-    		if ((IVs[0] + IVs[1] + IVs[2] + IVs[3] + IVs[4] + IVs[5])<186
-		 pivs = '"ivs":{'
-	}
-    
-  		if (document.getElementById("hcheck").checked) = true
-    		var p1 = '{"'
-    		var p2 = '":{"flags":{'
-    		var p3 = '},"sets":['
-    		var hcheck = p1.concat(species, p2, flags, p3)
-    		else hcheck = "";
-    
-    		var bcheck = "";
-    
-    		if (document.getElementById("fcheck").checked) = true
-    		var fcheck = ']},'
 		
 		
 	//var res = ln1p1.concat(species, ln1p2, ln2p1, ln2p2, ln2p3, ln3, ln4, hpev, atev, deev, saev, sdev, spev, ln11, ln12p1, hpiv, ativ, deiv, saiv, sdiv, spiv, ln11v2, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, move1, lnen, lnmv, move2, lnen, lnmv, move3, lnen, lnmv, move4, lnenl, ln3l, ln2l, lnl);
